924eafa73817a88496f081ba05e4e6ceaf2098e7
sunjing <sunjing@mzhcorp.com>
2022-09-19 17:14:20 +0800

    feat: counter selector and dispatch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import { useState } from 'react';
 import './App.css';
+import Counter from './Counter'
 
 function App() {
-  const [state, setState] = useState(0)
-
-  function increment() {
-    setState(state + 1)
-  }
-
-  function decrement() {
-    setState(state - 1)
-  }
 
   return (
     <div className="App">
       Hello, Redux
-      <div>
-        <button onClick={increment}>+</button>
-        <button onClick={decrement}>-</button>
-        <span>{state}</span>
-      </div>
+      <Counter/>
     </div>
   );
 }
