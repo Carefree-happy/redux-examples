1a9cbd619c707039cf82ac7ffcab0d0ccc27bd36
sunjing <sunjing@mzhcorp.com>
2022-09-20 23:58:58 +0800

    ci: create clean env

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { render } from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
-import Root from './containers/Root'
-import configureStore from './store/configureStore'
-
-const store = configureStore()
+import App from './App';
 
 render(
-  <Router>
-    <Root store={store} />
-  </Router>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 )
 
