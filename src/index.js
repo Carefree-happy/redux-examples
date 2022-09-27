d2228be3db90b591cf9cac1ebc01d2bfc26af791
sunjing <sunjing@mzhcorp.com>
2022-09-27 16:23:26 +0800

    feat: todo-with-undo

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import React from 'react'
 import { render } from 'react-dom'
-import App from './App'
+import App from './components/App'
+import reducer from './reducers'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore(reducer)
 
 render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
 
